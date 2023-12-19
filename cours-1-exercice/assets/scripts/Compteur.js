044ce84d828e7124df9aa8c85a6c1a4f18406157
René de Montigny <renedemontigny@MacBook-Air-de-Rene.local>
2023-12-19 15:28:00 -0500

    Ajouter la class: calcul fonctionel

--- cours-1-exercice/assets/scripts/Compteur.js
+++ cours-1-exercice/assets/scripts/Compteur.js
@@ -1,12 +1,78 @@
 //class Compteur
 
 class Compteur {
-    constructor() {
+    constructor(el) {
         // créer le this.'noeud DOM';
-        this.el = document.querySelector();
-        this.valeur = 0;
+        this._el = el;
+        this._elOperations = this._el.querySelector("[data-js-operations]");
+        this._elNombre = this._el.querySelector("[data-js-nombre]");
+        this._nombre = parseFloat(this._elNombre.textContent); // testContent prend le contenu de ce qu'il y a à cet endroit
+
+        this.init();
+    }
+    init() {
+        console.log(this._elOperations);
+
+        this._elOperations.addEventListener(
+            "click",
+            function (pointerEv) {
+                let action = pointerEv.target.dataset.jsAction;
+                if (action) {
+                    this.calculer(action);
+                    this.afficher(action);
+                }
+            }.bind(this)
+        );
+    }
+
+    calculer(operateur) {
+        if (operateur == "moins") this._nombre--;
+        if (operateur == "plus") this._nombre++;
+    }
+
+    afficher(action) {
+        this._elNombre.innerText = this._nombre;
     }
 }
 
 //Lopper dans les élément HTML, inclure un new compteur
 //une fois que 1 compteur fonctionne les autres devraient fonctionner
+
+/*
+
+1
+Vous avez cinq éléments DOM affichant un nombre, au clic d’un des deux 
+boutons à sa droite, faites le calcul (plus ou moins un) de ce nombre.
+Plus précisément :
+-
+Cours 1 - Exercice (formatif)
+
+1- Au chargement de la page, faites l’instanciation de la classe Compteur 
+pour chaque élément data-js-compteur en prenant soin de passer sa 
+référence DOM en argument.
+
+
+2- À l’intérieur de la classe Compteur, récupérez d’abord les éléments et 
+informations nécessaires aux comportements à suivre.
+
+
+3- Initialisez le(s) gestionnaire(s) d’événements.
+
+4- Suite au clic d’un bouton, faites le calcul approprié.
+
+5- Pour enrichir l’expérience usager, faites une transition de sortie du 
+nombre précédent puis une transition d’arrivée pour le nombre actualisé 
+nouvellement injecté. 
+
+Je suggère une courte transition du bas vers le 
+haut pour une addition et, inversement, du haut vers le bas pour une 
+soustraction. Pour ce faire, vous aurez à mettre à profit les propriétés et 
+méthodes de l’API DOM vues la session passée ainsi que l’événement 
+'transitionend'. 
+
+Notez également que vous aurez à définir les 
+différentes classes et propriétés CSS nécessaires aux transitions.
+Assurez-vous de factoriser votre code afin d’éviter la redondance et de 
+commenter dès que pertinent.
+-
+*/
--- cours-1-exercice/assets/scripts/main.js
+++ cours-1-exercice/assets/scripts/main.js
@@ -1,1 +1,28 @@
+"use strict";
+(function () {
+    function init() {
+        const elsCompteur = document.querySelectorAll("[data-js-compteur]");
 
+        for (let i = 0, l = elsCompteur.length; i < l; i++) {
+            new Compteur(elsCompteur[i]);
+        }
+
+        /* ajoutTexte(texte); */
+
+        /*         const textContentOutput = document.getElementById("textContentOutput");
+    const innerTextOutput = document.getElementById("innerTextOutput");
+
+    textContentOutput.value = source.textContent;
+    innerTextOutput.innerHTML = source; */
+
+        const h2 = document.getElementById("myH2");
+        let html = "<p>My new paragraph.</p>";
+        h2.insertAdjacentHTML("afterend", html);
+
+        const blocHTML = document.querySelectorAll(".test");
+        let source = "<p>test</p>";
+        h2.insertAdjacentHTML("afterend", source);
+    }
+
+    init();
+})();
