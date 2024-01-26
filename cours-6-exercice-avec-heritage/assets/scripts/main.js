44213fb7a9d00e503979e25a702ea8d527f8788c
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-01-26 10:16:58 -0500

    6-avec héritage: premier commit

---
+++ cours-6-exercice-avec-heritage/assets/scripts/main.js
@@ -0,0 +1,19 @@
+import { classesMapping } from './classesMapping.js';
+
+
+
+(function() {
+	
+	let elsComponent = document.querySelectorAll('[data-js-component]');
+	
+	for (let i = 0, l = elsComponent.length; i < l; i++) {
+		
+		let component = elsComponent[i].dataset.jsComponent,				// => string
+			elComponent = elsComponent[i];
+
+		for (let cle in classesMapping) {
+			if (component == cle) new classesMapping[component](elComponent);
+		}
+	}
+	…
+})();
