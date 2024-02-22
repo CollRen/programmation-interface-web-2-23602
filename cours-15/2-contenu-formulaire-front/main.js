9e8b6278e1d44c82232fc70942facd6d052a0157
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-02-22 15:18:55 -0500

    Contenu dynamique en loop

--- cours-15/2-contenu-formulaire-front/main.js
+++ cours-15/2-contenu-formulaire-front/main.js
@@ -1,5 +1,4 @@
 (function() {
-
 	let elUsager = document.querySelector('[data-js-usager]'),
 		elTemplateDetail = elUsager.querySelector('[data-template-detail]'),
 		elFormulaire = elUsager.querySelector('[data-js-formulaire]'),
@@ -7,12 +6,20 @@
 		elDetail = elUsager.querySelector('[data-js-detail]');
 
 
+
 	elBtn.addEventListener('click', function(e) { 
+
+
+
 		e.preventDefault();
 
 		// Validation FRONT
 		//if (validation.estValide) {
 
+		/**
+		 * Les clés vont correspondrent avec le {{ }} de la page index
+		 * ex { prenom }
+		 */
 			let infos = {
 				prenom: elFormulaire.prenom.value,
 				nom: elFormulaire.nom.value,
@@ -34,9 +41,27 @@
 	 */
 	function afficheDetail(infos) {
 
+		
+		// console.log(elCloneTemplate);
+/* 		elCloneTemplate.innerHTML = elCloneTemplate.innerHTML.replaceAll('{{prenom}}', infos.prenom);
+		elCloneTemplate.innerHTML = elCloneTemplate.innerHTML.replace('{{nom}}', infos.nom);
+		elCloneTemplate.innerHTML = elCloneTemplate.innerHTML.replace('{{courriel}}', infos.courriel); */
 
 
+		let elCloneTemplate = elTemplateDetail.cloneNode(true);
 
+		for (const cle in infos) {
+			let regExp = new RegExp('{{' + cle + '}}','g');
+
+			elCloneTemplate.innerHTML = elCloneTemplate.innerHTML.replace(regExp, infos[cle]);
+			
+			console.log(regExp);
+			console.log(infos[cle]);
+		}
+		
+		let elContent = document.importNode(elCloneTemplate.content, true)
+
+		elDetail.append(elContent);
 
 	}
 })();
